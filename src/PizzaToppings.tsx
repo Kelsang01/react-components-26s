9f473c350dadd6f1e4e217e1193fa40a8b9a2f98
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-04-07 16:03:18 -0500

    Pizza Toppings Placeholders...

--- src/PizzaToppings.tsx
+++ src/PizzaToppings.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { dummyPizzaToppingsFromApi } from "./dummy-pizza-toppings";
 
 const PizzaToppings = () => {
-   return (
 
+  // React hooks...
+  const [availablePizzaToppings, setAvailablePizzaToppings] = useState(
+    dummyPizzaToppingsFromApi.map(
+      x => ({
+        ...x,
+        checked: false,
+      })
+    )
+  );
+
+  // Calculate derived state and other code...
+
+
+  // Return JSX...
+
+    return (
     <Card>
       <Card.Body>
       <Card.Title>
@@ -21,9 +38,20 @@ const PizzaToppings = () => {
       >
         None
       </Button>
-      <div>
-        Pizza toppings go here...
-      </div>
+      {
+        availablePizzaToppings.map(
+            x => (
+                  <div
+                  className="my-2"
+                  >
+                  {
+                    `${x.name} (${x.price})`
+                  }
+                  </div>
+            )
+         )
+      }
+
       <h3>
         Total: $0.00
       </h3>
