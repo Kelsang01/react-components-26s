938b18d3d3ac5fca7f2974b1e4eebdc2786c0275
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-11 15:53:10 -0500

    Changed useRef to useState for tracking user input

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { useState } from "react";
 
 import Button from "react-bootstrap/Button";
@@ -7,7 +6,7 @@ import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 0
 const Welcome = () => {
-    const name = useRef<HTMLInputElement | null>(null);
+    const [name, setName] = useState("");
     const [welcomeMessage, setWelcomeMessage] = useState("");
 
     return (
@@ -17,11 +16,25 @@ const Welcome = () => {
 
                 <InputGroup className="mb-4">
                     <InputGroup.Text>Name:</InputGroup.Text>
-                    <Form.Control className="w-25" id="name" ref={name} type="text"></Form.Control>
+                    <Form.Control 
+                        className="w-25" 
+                        id="name" 
+                        onChange={
+                            event => {
+                                setName(event.target.value);
+                            }
+                        }
+                        type="text"
+                        value={name}
+                    >            
+                    </Form.Control>
                 </InputGroup>
 
                 <Button onClick={
-                            () => setWelcomeMessage(`Hello, ${name.current.value}!`)                             
+                            () => {
+                                setWelcomeMessage(`Hello, ${name}!`);
+                                setName("");
+                            }
                         }
                         variant="outline-primary"
                 >
@@ -29,14 +42,17 @@ const Welcome = () => {
                 </Button>
                 <Button className="ms-2" 
                         onClick={
-                            () => setWelcomeMessage("")
+                            () => {
+                                setWelcomeMessage("");
+                                setName("");
+                            }
                         }
                         variant="outline-secondary"
                 >
                     Reset
                 </Button>
 
-                <span className="ms-5">{`${welcomeMessage}`}</span>
+                <span className="ms-5" style={{fontWeight: 900}}>{`${welcomeMessage}`}</span>
             </Card.Body>
         </Card>
     );
