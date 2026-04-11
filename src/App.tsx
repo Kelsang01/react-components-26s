6eb74f1a4ecec4ebd4ce3dfe12ca9735a3503af6
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-11 09:51:09 -0500

    Created and displayed Pizza Toppings component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const PizzaToppings = () => {
+  return (
+    <h2>Choose your pizza toppings...</h2>
+  );
+};
+
 const App = () => {
   return (
     <div className="m-3">
       <h1>
         React Components
       </h1>
+      <PizzaToppings />
     </div>
   );
-}
+};
 
 export default App
