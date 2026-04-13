0d464dc451ae9086de0674330ac71a50a1495f26
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-13 16:17:42 -0500

    All none working, and total calculated with m / f / r.

--- src/PizzaToppings.tsx
+++ src/PizzaToppings.tsx
@@ -17,6 +17,9 @@ const PizzaToppings = () => {
   //
   //Calculate derived state, and other code...
   //
+  const total = availablePizzaToppings
+    .filter((x) => x.checked)
+    .reduce((acc, x) => acc + x.price, 0);
 
   //
   //Return JSX...
@@ -25,14 +28,37 @@ const PizzaToppings = () => {
     <Card>
       <Card.Body>
         <Card.Title className="mb-3">Choose your pizza toppings...</Card.Title>
-        <Button variant="outline-primary">All</Button>
-        <Button className="ms-2" variant="outline-primary">
+        <Button
+          variant="outline-primary"
+          onClick={() =>
+            setAvailablePizzaToppings(
+              availablePizzaToppings.map((x) => ({
+                ...x,
+                checked: true,
+              })),
+            )
+          }
+        >
+          All
+        </Button>
+        <Button
+          className="ms-2"
+          variant="outline-primary"
+          onClick={() =>
+            setAvailablePizzaToppings(
+              availablePizzaToppings.map((x) => ({
+                ...x,
+                checked: false,
+              })),
+            )
+          }
+        >
           None
         </Button>
         {availablePizzaToppings.map((x) => (
-          <div className="my-2">{`${x.name} (${x.price})`}</div>
+          <div className="my-2">{`${x.name} ($${x.price.toFixed(2)})`}</div>
         ))}
-        <h3>Total: $0.00</h3>
+        <h3>Total: {`$${total.toFixed(2)}`}</h3>
       </Card.Body>
     </Card>
   );
