952e94e3a43f3507eeb6f31110439ec2273b17bf
Shaun Khang <khaunx02@gmail.com>
2026-05-06 14:33:32 -0500

    Created pizza toppings component
    Next we'll move it to it's own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const PizzaToppings = () => <h2>Choose your pizza toppings...</h2>;
+
 const App = () => {
   return (
     <div className="m-3">
       <h1>React Components</h1>
+      <PizzaToppings />
     </div>
   );
 };
