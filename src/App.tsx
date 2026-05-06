2517988f5c7914d5950d8c39b6492c5edde1b536
Shaun Khang <khaunx02@gmail.com>
2026-05-06 15:51:14 -0500

    Completion of increment progress bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PizzaToppings from './PizzaToppings';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PizzaToppings from "./PizzaToppings";
+import SkhangComponent from "./SkhangComponent";
 
 const App = () => {
-
   return (
-    <div
-      className='m-3'
-    >
-      <h1>
-        React Components
-      </h1>
+    <div className="m-3">
+      <h1>React Components</h1>
       <PizzaToppings />
+      <SkhangComponent />
     </div>
   );
-}
+};
 
-export default App
+export default App;
